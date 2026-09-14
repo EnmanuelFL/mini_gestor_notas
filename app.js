e78c58cef320f9a4bba90d17e722edf7b9164668
Enmanuel Feliciano <enmanuellemos.f@gmail.com>
2026-09-14 02:55:03 +0200

    funcionalidad de crear hecha

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const inputNota = document.querySelector('#descripcion_nota')
 const buttoNota = document.querySelector('#añadir_nota')
-let notas_generads = document.querySelector('.notas_generadas')
+let notas_generadas = document.querySelector('.notas_generadas')
 let notas = []
 
 document.addEventListener("DOMContentLoaded", () => {
-    guardarConfiguracionInicial();
+    ConfiguracionInicial();
 });
 buttoNota.addEventListener('click', pagina)
 function pagina (){
+    if (!inputNota.value.trim()) return;
     let nota = {    
         id: Date.now(),
         descripcion: inputNota.value,
         fecha: new Date(),
-        importante: false
+        importante: true
     }
     notas.push(nota)
     inputNota.value = ""
+    Notas_renderizadas()
+}
+
+function Notas_renderizadas (){
+    notas_generadas.innerHTML = ""
+    notas.forEach((nota)=>{
+        const contenedor = document.createElement("article");
+        const texto = document.createElement("p");
+        texto.innerText = nota.descripcion;
+        const fecha = document.createElement("small");
+        fecha.innerText = nota.fecha.toLocaleDateString();
+        const botonImportante = document.createElement("button");
+        botonImportante.innerText = nota.importante ? "Importante: Sí" : "Marcar importante";
+        const botonEliminar = document.createElement("button");
+        botonEliminar.innerText = "Eliminar";
+        
+        contenedor.appendChild(texto);
+        contenedor.appendChild(fecha);
+        contenedor.appendChild(botonImportante);
+        contenedor.appendChild(botonEliminar);
+        notas_generadas.appendChild(contenedor);
+    });
+}
+
+function local_storage () {
+    localStorage.setItem('mis_notas', JSON.stringify(notas));
 }
